1439fdaf346e87b583337e74a1c8db9a81627f4b
DriveSP <gtavvice@hotmail.es>
2025-04-26 14:26:57 +0200

    Random Cards
    Random Cards

--- Assets/Scripts/CardController.cs
+++ Assets/Scripts/CardController.cs
@@ -1,9 +1,14 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CardController : MonoBehaviour
 {
     private Boolean cardFlipped;
+    public string cardName;
+    public string description;
+    public List<Card> strongs;
+    public List<Card> weaks;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,20 +2,26 @@ using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private int hp;
     [SerializeField]private List<Card> deck;
-    private List<Card> hand;
-    private List<GameObject> cardPrefab;
+    [SerializeField] GameObject cardPrefab;
+    private Transform deckTransform;
+    private List<Card> hand = new List<Card>();
+    private List<GameObject> cardPrefabList;
+    private Animator animatorCard;
     private Card cardPlayed;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        deckTransform = this.transform;
     }
 
     // Update is called once per frame
@@ -27,16 +33,35 @@ public class PlayerController : MonoBehaviour
     public void ObtainCards()
     {
         List<Card> randomCards = GetRandomCards(deck, 3);
+        int cardCount = 1;
         foreach (var card in randomCards)
         {
             Debug.Log(card.name);
             hand.Add(card);
+            GameObject newCard = Instantiate(cardPrefab, deckTransform);
+            newCard.transform.localPosition = Vector3.zero;
+            FillDataCard(card, newCard);
+
+
+            Animator animatorCard = newCard.GetComponent<Animator>();
+
+            if (animatorCard != null)
+            {
+                animatorCard.Play("Card"+cardCount);
+            }
+            else
+            {
+                Debug.LogWarning("Animator isn't exist");
+            }
+            cardCount++;
         }
     }
 
-    private void FillDataCard(Card card)
+    private void FillDataCard(Card card, GameObject cardPrefab)
     {
-
+        CardController cardController = cardPrefab.GetComponent<CardController>();
+        TextMeshProUGUI titleCard = cardPrefab.GetComponentInChildren<TextMeshProUGUI>();
+        titleCard.text = card.name;
     }
 
     private List<Card> GetRandomCards(List<Card> source, int count)
